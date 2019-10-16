6bb232e9ba3da222ebf7ad7ec5598aef587c6c03
Dallas James <dallasjames42@gmail.com>
2019-10-15 22:22:30 -0600

    prevwork and contact me done

---
+++ resume/src/PrevWork.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function PrevWork() {
+    return (
+        <>
+            <h1>Previous Work</h1>
+            <div className='dish'>
+                <h2>Dishwashing</h2>
+                <h3>Chili's Boise/Meridian Idaho</h3>
+                <h4>Skills Learned</h4>
+                <p>This job taught me to work as part of a team and helped develope my persistence and work ethic.</p>
+                <h4>Date's worked</h4>
+                <p>November 2015 - May 2017</p>
+            </div>
+            <div className='apple'>
+                <h2>Apple Support</h2>
+                <h3>Teleperformance Boise Idaho</h3>
+                <h4>Skills Learned</h4>
+                <p>This job taught me the stick-to-itiveness skills that coinside with programming and to serve the customers needs.</p>
+                <h4>Date's worked</h4>
+                <p>June 2017 - Current</p>
+            </div>
+        </>
+    )
+}
+
+export default PrevWork;
