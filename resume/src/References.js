7d686de2b74680ab36b6f7faadee01c52703bbf0
Dallas James <dallasjames42@gmail.com>
2019-10-16 17:54:53 -0600

    stock references updated

--- resume/src/References.js
+++ resume/src/References.js
@@ -5,9 +5,9 @@ export function References() {
         <>
             <h1>References</h1>
             <h3>Tabitha Dovel</h3>
-            <a href='tel+1-555-555-5555'>+1-555-555-5555</a>
+            <p>Phone: <a href='tel+1-555-555-5555'>+1-555-555-5555</a></p>
             <h3>Giovanni Sanguinetti</h3>
-            <a href='tel*1-555-555-5555'>*1-555-555-5555</a>
+            <p>Phone: <a href='tel*1-555-555-5555'>+1-555-555-5555</a></p>
         </>
     )
 }
