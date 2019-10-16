16ca290ad22546e70ec2be9ce842aed434a7bda1
Dallas James <dallasjames42@gmail.com>
2019-10-15 22:40:29 -0600

    skills done

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Contact } from './Contact';
 import PrevWork from './PrevWork';
+import Skills from './Skills';
 
 function App() {
   return (
     <>
     <h1>Dallas James</h1>
+    <Skills />
     <PrevWork />
     <Contact />
     </>    
---
+++ resume/src/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Skills() {
+    return (
+        <>
+            <h1>Technical Skills</h1>
+            <h4>Semantic HTML5, CSS3, LESS, SASS, JavaScript, JSX, React(class components and hooks), Axios,
+                C, Python, Basic, Ducky Script, Bunny Script, Linux(Kali, Mint, Ubuntu), Mac(El Capitan through 
+                Catalina), Windows(7, 8, 8.1, 10) Open-Source Projects, Trello, Arduino, Photoshop</h4>
+        </>
+    )
+}
+
+export default Skills
