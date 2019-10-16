23cf5eae2f766a96c57d1f2fb2f02b0e142b10c5
Dallas James <dallasjames42@gmail.com>
2019-10-15 22:51:57 -0600

    dark mode switch in no css yet though

--- resume/src/App.js
+++ resume/src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { Contact } from './Contact';
 import PrevWork from './PrevWork';
 import Skills from './Skills';
+import useDarkMode from './Hooks/useDarkMode';
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode();
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
     <h1>Dallas James</h1>
+      <div
+        onClick={toggleMode}
+        className={darkMode ? 'dark' : 'light'}
+      >Dark Mode</div>
+    <h2>For a job done well and in time.</h2>
     <Skills />
     <PrevWork />
     <Contact />
